fcd91bf62d5c6750e52df40da12af25e94592da4
Winston Lin <winstonnlin@gmail.com>
2013-02-19 21:15:34 -0500

    Added Edit Menu and Take Option activiites

--- gen/edu/upenn/medicinereminder/R.java
+++ gen/edu/upenn/medicinereminder/R.java
@@ -37,17 +37,21 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_appointments=0x7f030000;
-        public static final int activity_home=0x7f030001;
-        public static final int activity_medication=0x7f030002;
-        public static final int activity_set_reminder=0x7f030003;
-        public static final int activity_user_info=0x7f030004;
+        public static final int activity_edit_menu=0x7f030001;
+        public static final int activity_home=0x7f030002;
+        public static final int activity_medication=0x7f030003;
+        public static final int activity_set_reminder=0x7f030004;
+        public static final int activity_take_option=0x7f030005;
+        public static final int activity_user_info=0x7f030006;
     }
     public static final class menu {
         public static final int activity_appointments=0x7f070000;
-        public static final int activity_home=0x7f070001;
-        public static final int activity_medication=0x7f070002;
-        public static final int activity_set_reminder=0x7f070003;
-        public static final int activity_user_info=0x7f070004;
+        public static final int activity_edit_menu=0x7f070001;
+        public static final int activity_home=0x7f070002;
+        public static final int activity_medication=0x7f070003;
+        public static final int activity_set_reminder=0x7f070004;
+        public static final int activity_take_option=0x7f070005;
+        public static final int activity_user_info=0x7f070006;
     }
     public static final class string {
         public static final int MEDCHECK=0x7f050005;
---
+++ src/edu/upenn/medicinereminder/EditMenu.java
@@ -0,0 +1,7 @@
+package edu.upenn.medicinereminder;
+
+import android.app.Activity;
+
+public class EditMenu extends Activity{
+
+}
