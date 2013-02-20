2d9e8460c30244b539e2f0c88c496417eb0dcce5
Min H Lee <lemin@seas.upenn.edu>
2013-02-19 21:31:29 -0500

    d

--- gen/edu/upenn/medicinereminder/R.java
+++ gen/edu/upenn/medicinereminder/R.java
@@ -38,13 +38,15 @@ public final class R {
         public static final int activity_appointments=0x7f030000;
         public static final int activity_home=0x7f030001;
         public static final int activity_set_reminder=0x7f030002;
-        public static final int activity_user_info=0x7f030003;
+        public static final int activity_setrefills=0x7f030003;
+        public static final int activity_user_info=0x7f030004;
     }
     public static final class menu {
         public static final int activity_appointments=0x7f070000;
         public static final int activity_home=0x7f070001;
         public static final int activity_set_reminder=0x7f070002;
-        public static final int activity_user_info=0x7f070003;
+        public static final int activity_setrefills=0x7f070003;
+        public static final int activity_user_info=0x7f070004;
     }
     public static final class string {
         public static final int MEDCHECK=0x7f050005;
